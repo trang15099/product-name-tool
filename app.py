eae9c1fdebe6d3dc17cfd25c48e025ddc45ec36c
trang15099 <72885883+trang15099@users.noreply.github.com>
2025-08-24 16:18:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -416,18 +416,18 @@ group = st.selectbox(
     placeholder="Chọn nhóm…"
 )
 
-# 📤 Upload file
-uploaded = st.file_uploader("Upload specsheet (.xlsx)", type=["xlsx"])
 
 # ⛔️ Yêu cầu: phải có file + đã chọn nhóm
-"""
 if uploaded is None or group is None:
-    if uploaded is None:
-        st.info("⬆️ Upload file Excel specsheet")
+    
     if group is None:
-        st.info("🔽 Chọn nhóm sản phẩm")
+        st.info("🔽⬆️ Chọn nhóm sản phẩm")
+    if uploaded is None:
+        st.info("🔽 Upload file Excel specsheet")
     st.stop()
-"""
+
+# 📤 Upload file
+uploaded = st.file_uploader("Upload specsheet (.xlsx)", type=["xlsx"])
 
 # ✅ Đủ điều kiện -> xử lý
 raw_df = pd.read_excel(uploaded, header=None)
@@ -444,6 +444,7 @@ with st.expander("👀 Xem nhanh file input"):
     st.dataframe(raw_df)
 with st.expander("🛠 Keys đã đọc (debug)"):
     st.write(kv)
+
 
 
 
